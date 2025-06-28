ca6ac5e02210a85df5aa34b831be88c7dc4b663f
shakibsabka <bithi3903@gmail.com>
2025-06-28 22:45:39 +0600

    Create git-setup.sh

---
+++ git-setup.sh
@@ -0,0 +1,6 @@
+cd /path/to/your/panel
+git init
+git remote add origin https://github.com/shakib112/shakib-panel.git
+git add .
+git commit -m "🎉 Initial release of Shakib Panel"
+git push -u origin master
